130fa57e118f83751d980c8a2e9fa66fefdf8f83
Camilo Ossa <ingcamilo.ossa@gmail.com>
2024-02-14 20:09:33 -0500

    Estilos del HeroPage una vez se consulte el personaje finalizados

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -5,36 +5,40 @@ import { useMemo } from 'react';
 const HeroPage = () => {
 
   const { id } = useParams();
-  const publisher = id.split('-');  
+  const publisher = id.split('-');
 
-  const hero = useMemo( () => getHeroById( id ), [ id ] ); 
+  const hero = useMemo(() => getHeroById(id), [id]);
 
   const navigate = useNavigate();
 
-  if( !hero ) {
-    return <Navigate to = {`/${publisher[0]}`} />
+  if (!hero) {
+    return <Navigate to={`/${publisher[0]}`} />
   }
 
   return (
-    <div className="row mt-5">
-        <div className="col-sm-4 col-12 animate__animated animate__fadeInLeft animate__faster">
-          <img src={`/heroes/${ id }.jpg`} className="img-thumbnail " alt={ hero.superhero } />
-        </div>
+    <div className="heroe-page">
+      <div className="heroe-page__img">
+        <img src={`/heroes/${id}.jpg`} class="heroe-page__img-heroe" alt={hero.superhero} />
+      </div>
 
-        <div className="col-sm-8 col-12 animate__animated animate__fadeInRight animate__faster">
-          <h3>{ hero.superhero }</h3>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item"> <b>Alter ego: </b> { hero.alter_ego }</li>
-            <li className="list-group-item"> <b>Publisher: </b> { hero.publisher }</li>
-            <li className="list-group-item"> <b>First Appearcen: </b> { hero.first_appearance }</li>
+      <div className="heroe-page__caja">
+        <h3 className="heroe-page__heading">{hero.superhero}</h3>
+        <div className="heroe-page__body">
+          <ul className="heroe-page__descripcion">
+            <li className="heroe-page__item"> <b>Alter ego: </b> {hero.alter_ego}</li>
+            <li className="heroe-page__item"> <b>First Appearcen: </b> {hero.first_appearance}</li>
+            <li className="heroe-page__item"> <b>Publisher: </b> {hero.publisher}</li>
           </ul>
-          <h5 className="mt-3">Characters</h5>
-          <p>{ hero.characters }</p>
-
-          <button className="btn btn-outline-info" onClick={ () => navigate(-1) }>Regresar</button>
+          <h5 className="heroe-page__titulo">Characters</h5>
+          <p className="heroe-page__texto">{hero.characters}</p>
         </div>
+
+        <div className="heroe-page__footer">
+          <button className="heroe-page__btn" onClick={() => navigate(-1)}>Regresar</button>
+        </div>
+      </div>
     </div>
   )
 }
 
-export  { HeroPage }
+export { HeroPage }
